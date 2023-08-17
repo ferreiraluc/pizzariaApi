2f0f4aef821945f64a725b02272a054639aab70d
MGPKrohn <matheuskrohn@gmail.com>
2023-08-17 18:33:48 -0300

    Updt Produto

--- pizzariaApi/src/main/java/com/lm/pizzariaApi/repository/ProdutoRepository.java
+++ pizzariaApi/src/main/java/com/lm/pizzariaApi/repository/ProdutoRepository.java
@@ -1,14 +1,10 @@
 package com.lm.pizzariaApi.repository;
 
-import com.lm.pizzariaApi.entity.Pedido;
 import com.lm.pizzariaApi.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    @Override
-    Optional<Produto> findById(Long Id);
+
 }
--- pizzariaApi/src/main/java/com/lm/pizzariaApi/service/ProdutoService.java
+++ pizzariaApi/src/main/java/com/lm/pizzariaApi/service/ProdutoService.java
@@ -2,40 +2,51 @@ package com.lm.pizzariaApi.service;
 
 import com.lm.pizzariaApi.entity.Produto;
 import com.lm.pizzariaApi.entity.Tamanho;
+import com.lm.pizzariaApi.repository.ClienteRepository;
 import com.lm.pizzariaApi.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProdutoService {
+
     private final ProdutoRepository produtoRepository;
+    private final ClienteRepository clienteRepository;
+
     @Autowired
-    public ProdutoService(ProdutoRepository produtoRepository) {
+    public ProdutoService(ProdutoRepository produtoRepository,
+                          ClienteRepository clienteRepository) {
         this.produtoRepository = produtoRepository;
+        this.clienteRepository = clienteRepository;
     }
 
-    public Produto BuscarPorId(Long id){return produtoRepository.findById(id).orElse(null);}
+    public Produto findById(Long id){return produtoRepository.findById(id).orElse(null);}
 
-    public void DeletarProduto(Long produtoId){
-        Produto produto = produtoRepository.findById(produtoId)
-                .orElseThrow(() -> new RuntimeException("Produto Não Encontrado"));
-        produtoRepository.delete(produto);
-    }
+    public Produto criarProduto(Produto produto){
+        Tamanho tamanho = produto.getTamanho();
 
-    public Produto CriarProduto(Produto produto){
-        if (produto.getTamanho() == Tamanho.valueOf("P")){
+        // Definindo o preço com base no tamanho da pizza
+        if (tamanho == Tamanho.P) {
             produto.setProduto_preco(50);
-        }
-        if (produto.getTamanho() == Tamanho.valueOf("M")){
-            produto.setProduto_preco(70);
-        }
-        if (produto.getTamanho() == Tamanho.valueOf("G")){
-            produto.setProduto_preco(85);
-        }
-        if (produto.getTamanho() == Tamanho.valueOf("GG")){
+            produto.setProduto_item("Sabor único");
+        } else if (tamanho == Tamanho.M) {
+            produto.setProduto_preco(60);
+            produto.setProduto_item("Dois sabores");
+        } else if (tamanho == Tamanho.G) {
+            produto.setProduto_preco(80);
+            produto.setProduto_item("Três sabores");
+        } else if (tamanho == Tamanho.GG) {
             produto.setProduto_preco(100);
+            produto.setProduto_item("Três sabores");
         }
-        return null;
+
+        return produtoRepository.save(produto);
+    }
+
+    public void deletarProduto(Long produtoId){
+        Produto produto = produtoRepository.findById(produtoId)
+                .orElseThrow(() -> new RuntimeException("Produto não Encontrado"));
+        produtoRepository.delete(produto);
     }
 
 }
