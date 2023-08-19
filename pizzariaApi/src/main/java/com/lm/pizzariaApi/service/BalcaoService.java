1330c7533238d63fd3dc5bf9312c26c72216e01f
lucas.gimenez <l.adrianobf@gmail.com>
2023-08-18 22:06:37 -0300

    update Balcao

--- pizzariaApi/src/main/java/com/lm/pizzariaApi/service/BalcaoService.java
+++ pizzariaApi/src/main/java/com/lm/pizzariaApi/service/BalcaoService.java
@@ -3,6 +3,7 @@ package com.lm.pizzariaApi.service;
 import com.lm.pizzariaApi.entity.Pedido;
 import com.lm.pizzariaApi.entity.Produto;
 import com.lm.pizzariaApi.repository.PedidoRepository;
+import com.lm.pizzariaApi.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
@@ -16,6 +17,9 @@ public class BalcaoService {
 
     @Autowired
     private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
     public List<Pedido> visualizarTodosPedidos() {
         List<Pedido> pedidos = pedidoRepository.findAll();
@@ -32,6 +36,27 @@ public class BalcaoService {
         }
         return finalizados;
     }
+
+    public List<Pedido> visualizarPedidosPendentes() {
+        List<Pedido> pendentes = pedidoRepository.findByStatus("pendente");
+        if (pendentes.isEmpty()) {
+            logger.info("Não foi encontrado nenhum pedido pendente.");
+        } else {
+            logger.info("Visualizando pedidos pendentes: {}", pendentes);
+        }
+        return pendentes;
+    }
+
+    public List<Pedido> visualizarPedidosEntregues() {
+        List<Pedido> entregues = pedidoRepository.findByStatus("entregue");
+        if (entregues.isEmpty()) {
+            logger.info("Não foi encontrado nenhum pedido entregue.");
+        } else {
+            logger.info("Visualizando pedidos entregues: {}", entregues);
+        }
+        return entregues;
+    }
+
 
 
     // Outros métodos para visualização conforme necessário...
