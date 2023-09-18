c0e762c8abbed3bab7611d329c549864690e54c7
MGPKrohn <matheuskrohn@gmail.com>
2023-09-18 20:57:27 -0300

    Testes Balcao

--- pizzariaApi/src/main/java/com/lm/pizzariaApi/entity/Balcao.java
+++ pizzariaApi/src/main/java/com/lm/pizzariaApi/entity/Balcao.java
@@ -1,10 +1,25 @@
 package com.lm.pizzariaApi.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Balcao",schema = "public")
 public class Balcao extends AbstractEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "pedidos_id", referencedColumnName = "id")
+    private Pedido pedido;
+
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", referencedColumnName = "id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "funcionario_id", referencedColumnName = "id")
+    private Funcionario funcionario;
 
 }
