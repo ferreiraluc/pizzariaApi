396dded7b19b459d736cb7050abd66b4b92dacb0
lucas.gimenez <l.adrianobf@gmail.com>
2023-09-19 21:37:41 -0300

    update FuncionarioControllerTest

--- pizzariaApi/src/main/java/com/lm/pizzariaApi/controller/Testes/BalcaoTeste.java
+++ pizzariaApi/src/main/java/com/lm/pizzariaApi/controller/Testes/BalcaoTeste.java
@@ -1,43 +1,41 @@
 package com.lm.pizzariaApi.controller.Testes;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lm.pizzariaApi.controller.BalcaoController;
 import com.lm.pizzariaApi.entity.Pedido;
 import com.lm.pizzariaApi.service.BalcaoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 @WebMvcTest(BalcaoController.class)
 public class BalcaoTeste {
+
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private BalcaoService balcaoService;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @BeforeEach
-    public void inicializar(){}
+    public void inicializar() {}
 
     @Test
-    public void TesteVisualizarTodosOsPedidos()throws Exception{
+    public void TesteVisualizarTodosOsPedidos() throws Exception {
         List<Pedido> pedidosEsperados = new ArrayList<>();
 
         Pedido pedido1 = new Pedido();
-
         pedido1.setPedido_ID(1L);
         pedido1.setEntrega(true);
         pedido1.setStatus("Finalizados");
@@ -48,11 +46,12 @@ public class BalcaoTeste {
 
         pedidosEsperados.add(pedido1);
 
-        Mockito.when(balcaoService.visualizarTodosPedidos()).thenReturn(pedidosEsperados);
+        when(balcaoService.visualizarTodosPedidos()).thenReturn(pedidosEsperados);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/balcao")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-
+        mockMvc.perform(get("/api/balcao")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                // Adicionando uma verificação de conteúdo, assumindo que sua resposta é um JSON do pedido
+                .andExpect(content().json("[{\"pedido_ID\":1,\"entrega\":true,\"status\":\"Finalizados\",\"observacao\":\"Observação\",\"cliente_ID\":2,\"funcionario_ID\":3,\"produto_ID\":5}]"));
     }
 }
--- pizzariaApi/src/main/java/com/lm/pizzariaApi/entity/Balcao.java
+++ pizzariaApi/src/main/java/com/lm/pizzariaApi/entity/Balcao.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Entity
 @Table(name = "Balcao",schema = "public")
 public class Balcao extends AbstractEntity {
