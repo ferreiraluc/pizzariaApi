9e1af1cd9765b83e7da27601c95ca89bda7c93a3
lucas.gimenez <l.adrianobf@gmail.com>
2023-10-17 20:08:56 -0300

    update

--- pizzariaApi/src/test/java/com/lm/apipizzaria/Autowired.java
+++
@@ -1,4 +0,0 @@
-package com.lm.apipizzaria;
-
-public @interface Autowired {
-}
--- pizzariaApi/src/test/java/com/lm/apipizzaria/PizzariaApiApplicationTests.java
+++ pizzariaApi/src/test/java/com/lm/apipizzaria/PizzariaApiApplicationTests.java
@@ -8,6 +8,7 @@ class PizzariaApiApplicationTests {
 
     @Test
     void contextLoads(){
+        assert true;
     }
 
 }
