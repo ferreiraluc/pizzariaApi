67e268facf961c222c15bff5effc962fefd4b365
MGPKrohn <matheuskrohn@gmail.com>
2023-10-18 20:20:48 -0300

    Service Cliente

--- frontend/src/app/services/cliente.services.ts
+++ frontend/src/app/services/cliente.services.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject} from "@angular/core";
-import { HttpClient, Http} from "@angular/common/http";
+import { HttpClient, HttpParams} from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Cliente } from "../models/cliente";
 
@@ -7,10 +7,35 @@ import { Cliente } from "../models/cliente";
     providedIn: 'root'
 })
 
+
 export class ClienteService{
     API: string = 'http://localhost:8080/api/cliente';
     http = inject(HttpClient);
+
+
+constructor(){}
+
+    listAll(): Observable<Cliente[]>{
+        return this.http.get<Cliente[]>(this.API);
+    }
+
+    save(cliente: Cliente): Observable<Cliente>{
+        return this.http.post<Cliente>(this.API, Cliente);
+    }
+
+    byId(id: number): Observable<Cliente>{
+        const url = `${this.API}/${id}`;
+        return this.http.get<Cliente>(url);
+    }
+
+    delete(id: number): Observable<Cliente>{
+        const url = `${this.API}/${id}`;
+        return this.http.delete<Cliente>(url);
+    }
+
+    update(id: number, cliente: Cliente): Observable<Cliente>{
+        const url = `${this.API}/${id}`;
+        return this.http.put<Cliente>(url, cliente);
+    }
+
 }
-
-
-
