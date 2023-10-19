8caa20d4977cb531378ac6637e8f55aba29347a5
lucas.gimenez <l.adrianobf@gmail.com>
2023-10-18 22:12:27 -0300

    crud Produtos

--- frontend/src/app/components/produtos/produtosdetails/produtosdetails.component.ts
+++ frontend/src/app/components/produtos/produtosdetails/produtosdetails.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Produto } from 'src/app/models/produto';
+import { ProdutosService } from 'src/app/services/produtos.services';
 
 @Component({
   selector: 'app-produtosdetails',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class ProdutosdetailsComponent {
 
+  @Input() produto: Produto = new Produto();
+  @Output() retorno = new EventEmitter<Produto>();
+
+  produtosService = inject(ProdutosService);
+
+  constructor() { }
+
+  addProduto() {
+    this.produtosService.save(this.produto).subscribe({
+      next: produto => {
+        this.retorno.emit(produto);
+        alert('Produto adicionado com sucesso!');
+      },
+      error: erro => {
+        alert('Erro ao adicionar produto! Observe o erro no console!');
+        console.error(erro);
+      }
+    });
+  }
+
+
 }
+
+
